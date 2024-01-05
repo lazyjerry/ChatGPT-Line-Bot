8a5ef2388850bbabf136518ab51173956a0236a9
Lazy Jerry <susan.idea@gmail.com>
2024-01-05 19:07:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def handle_text_message(event):
     logger.info(f'{user_id}: {text}')
 
     try:
-        if text.startswith('/註冊'):
+        if text.startswith('/register'):
             api_key = text[3:].strip()
             model = OpenAIModel(api_key=api_key)
             is_successful, _, _ = model.check_token_valid()
@@ -68,21 +68,21 @@ def handle_text_message(event):
             })
             msg = TextSendMessage(text='Token 有效，註冊成功')
 
-        elif text.startswith('/指令說明'):
-            msg = TextSendMessage(text="指令：\n/註冊 + API Token\n👉 API Token 請先到 https://platform.openai.com/ 註冊登入後取得\n\n/系統訊息 + Prompt\n👉 Prompt 可以命令機器人扮演某個角色，例如：請你扮演擅長做總結的人\n\n/清除\n👉 當前每一次都會紀錄最後兩筆歷史紀錄，這個指令能夠清除歷史訊息\n\n/圖像 + Prompt\n👉 會調用 DALL∙E 2 Model，以文字生成圖像\n\n語音輸入\n👉 會調用 Whisper 模型，先將語音轉換成文字，再調用 ChatGPT 以文字回覆\n\n其他文字輸入\n👉 調用 ChatGPT 以文字回覆")
+        elif text.startswith('/help'):
+            msg = TextSendMessage(text="指令：\n/register + API Token\n👉 API Token 請先到 https://platform.openai.com/ 註冊登入後取得\n\n/cosplay + Prompt\n👉 Prompt 可以命令機器人扮演某個角色，例如：請你扮演擅長做總結的人\n\n/clear\n👉 當前每一次都會紀錄最後兩筆歷史紀錄，這個指令能夠清除歷史訊息\n\n/image + Prompt\n👉 以文字生成圖像\n\voice\n👉 將語音轉換成文字，再調用 ChatGPT 以文字回覆\n\n其他文字輸入\n👉 調用 ChatGPT 以文字回覆")
 
-        elif text.startswith('/系統訊息'):
+        elif text.startswith('/cosplay'):
             memory.change_system_message(user_id, text[5:].strip())
             msg = TextSendMessage(text='輸入成功')
 
-        elif text.startswith('/清除'):
+        elif text.startswith('/clear'):
             memory.remove(user_id)
             msg = TextSendMessage(text='歷史訊息清除成功')
 
-        elif text.startswith('/圖像'):
+        elif text.startswith('/image'):
             prompt = text[3:].strip()
             memory.append(user_id, 'user', prompt)
-            is_successful, response, error_message = model_management[user_id].image_generations(prompt)
+            is_successful, response, error_message = model_management[user_id].image_generations(prompt, os.getenv('OPENAI_MODEL_ENGINE_IMAGE'))
             if not is_successful:
                 raise Exception(error_message)
             url = response['data'][0]['url']
@@ -152,11 +152,11 @@ def handle_audio_message(event):
         if not model_management.get(user_id):
             raise ValueError('Invalid API token')
         else:
-            is_successful, response, error_message = model_management[user_id].audio_transcriptions(input_audio_path, 'whisper-1')
+            is_successful, response, error_message = model_management[user_id].audio_transcriptions(input_audio_path, os.getenv('OPENAI_MODEL_ENGINE_WHISPER'))
             if not is_successful:
                 raise Exception(error_message)
             memory.append(user_id, 'user', response['text'])
-            is_successful, response, error_message = model_management[user_id].chat_completions(memory.get(user_id), 'gpt-3.5-turbo')
+            is_successful, response, error_message = model_management[user_id].chat_completions(memory.get(user_id), os.getenv('OPENAI_MODEL_ENGINE'))
             if not is_successful:
                 raise Exception(error_message)
             role, response = get_role_and_content(response)
@@ -176,7 +176,7 @@ def handle_audio_message(event):
     line_bot_api.reply_message(event.reply_token, msg)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET','POST'])
 def home():
     return 'Hello World'
 
